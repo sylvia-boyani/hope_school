8d8e2f3866238f56d139686906acb181a37afeba
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2025-12-20 07:55:23 +0000

    home and navbar layout done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  protected
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_root_path
+    else
+      super
+    end
+  end
 end
