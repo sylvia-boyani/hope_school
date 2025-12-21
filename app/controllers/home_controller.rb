009f35ce4dbc43177bde1bc48c542a75b60b874a
Boyani <86007093+sylvia-boyani@users.noreply.github.com>
2025-12-21 20:33:14 +0000

    added images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,10 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def programs
+  end
+
+  def admissions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get "home/index"
   root "home#index"
   get "/about", to: "home#about"
+  get "/programs", to: "home#programs"
+  get "/admissions", to: "home#admissions"
 
   get "up" => "rails/health#show", as: :rails_health_checkqueries
 end
