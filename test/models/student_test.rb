d6b31a2b057df9b807c6c888e4288c90efaca36b
boyani-Otieno <boyani.ombogo7@gmailcom>
2026-01-21 11:55:27 +0300

    add tests for students model

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,32 +1,43 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  fixtures :levels, :class_rooms, :students
- def setup
-    @level = Level.create!(name: "Grade 1")
-    @class_room = ClassRoom.create!(level: @level, stream: "Red")
-    @student = Student.new(
-      first_name: "Jane", 
-      last_name: "Doe", 
-      admission_number: "1234", 
-      class_room: @class_room
-    )
+  def setup
+    @student = students(:jane)
   end
 
-  test "should be valid with all fields" do
+  test "fixture is valid" do
     assert @student.valid?
   end
 
-  test "invalid without first name" do
+  test "is invalid without first_name" do
     @student.first_name = nil
     assert_not @student.valid?
+    assert_includes @student.errors[:first_name], "can't be blank"
   end
 
-  test "name method returns full name" do
+  test "is invalid without last_name" do
+    @student.last_name = nil
+    assert_not @student.valid?
+    assert_includes @student.errors[:last_name], "can't be blank"
+  end
+
+  test "is invalid without admission_number" do
+    @student.admission_number = nil
+    assert_not @student.valid?
+    assert_includes @student.errors[:admission_number], "can't be blank"
+  end
+
+  test "belongs to a class_room" do
+    assert_not_nil @student.class_room
+  end
+
+  test "name returns full name" do
     assert_equal "Jane Doe", @student.name
   end
 
-  test "photo_thumbnail returns avatar URL when no photo attached" do
-    assert_match /ui-avatars.com/, @student.photo_thumbnail
+  test "photo_thumbnail returns avatar url when no photo attached" do
+    url = @student.photo_thumbnail
+    assert_match(/ui-avatars.com/, url)
+    assert_match(/Jane\+Doe/, url)
   end
 end
