1d6a695aa712ec181ba94219381c538c8841717d
boyani-Otieno <boyani.ombogo7@gmailcom>
2026-01-21 15:23:13 +0300

    add class_room tests

--- app/models/class_room.rb
+++ app/models/class_room.rb
@@ -5,6 +5,9 @@ class ClassRoom < ApplicationRecord
   has_many :teaching_assignments
   has_many :teachers, through: :teaching_assignments
 
+  validates :stream, presence: true 
+  validates :stream, uniqueness: { scope: :level_id }
+
   def grade_and_stream
     "#{level.name} - #{stream}"
   end
--- test/models/class_room_test.rb
+++ test/models/class_room_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class ClassRoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @class_room = class_rooms(:grade1_red)
+  end
+
+  test "should be valid" do
+    assert @class_room.valid?
+  end
+
+  test "should belong to a level" do
+    assert_not_nil @class_room.level
+    assert_equal "Grade 1", @class_room.level.name
+  end
+
+  test "should have students" do
+    # This works because we linked 'jane' to 'grade1_red' in fixtures
+    assert @class_room.students.count > 0
+    assert_includes @class_room.students, students(:jane)
+  end
+
+  test "should have teachers through assignments" do
+    # This works if you have an entry in teaching_assignments.yml
+    assert_includes @class_room.teachers, teachers(:teacher_one)
+  end
+
+  test "invalid without stream" do 
+    @class_room.stream = nil       
+    assert_not @class_room.valid?
+  end
 end
