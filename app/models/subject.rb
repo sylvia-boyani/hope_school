91162695bdc24e36b46c88e01fbf536ac6d4ea5e
boyani-Otieno <boyani.ombogo7@gmailcom>
2026-01-22 12:21:44 +0300

    add subject tests

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,8 +1,9 @@
 class Subject < ApplicationRecord
-  has_many :grade_subjects, dependent: :destroy
-  has_many :levels, through: :grade_subjects
   has_many :courses, dependent: :destroy
   has_many :levels, through: :courses
+  has_many :teaching_assignments
+  has_many :teachers, through: :teaching_assignments
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true
 end
--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @subject = subjects(:math)
+  end
+
+  test "should be valid" do
+    assert @subject.valid?
+  end
+
+  test "name should be present" do
+    @subject.name = ""
+    assert_not @subject.valid?
+  end
+
+  test "code should be unique" do
+    duplicate_subject = @subject.dup
+    @subject.save
+    assert_not duplicate_subject.valid?
+  end
+
+  test "should have many levels through courses" do
+    # This verifies the Subject -> Course -> Level connection
+    assert @subject.levels.include?(levels(:grade_1))
+  end
+
+  test "should have many teaching_assignments" do
+    # Verifies that we can see which teachers are assigned to this subject
+    assert_respond_to @subject, :teaching_assignments
+  end
 end
