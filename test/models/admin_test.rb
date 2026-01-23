ef4b8320eec2ffc6e9896c23c5e8c09ca4a69005
boyani-Otieno <boyani.ombogo7@gmailcom>
2026-01-23 19:33:29 +0300

    added admin tests

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = admins(:super_admin)
+  end
+
+  test "should be valid" do
+    assert @admin.valid?
+  end
+
+  test "should be invalid without email" do
+    @admin.email = ""
+    assert_not @admin.valid?
+  end
+
+  test "should be invalid with a malformed email" do
+    @admin.email = "not_an_email"
+    assert_not @admin.valid?
+  end
+
+  test "email should be unique" do
+    duplicate_admin = @admin.dup
+    assert_not duplicate_admin.valid?
+  end
+
+  test "password should be required" do
+    # Creating a new admin without a password
+    new_admin = Admin.new(email: "new@hopeschool.com")
+    assert_not new_admin.valid?
+  end
 end
