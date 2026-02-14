a2f83241132732a88ae53b83952438eb7df88bd3
boyani-Otieno <boyani.ombogo7@gmailcom>
2026-02-14 19:50:12 +0300

    add admin creation methodin seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+puts "Creating Admin"
+Admin.find_or_create_by!(email: 'admin@hopeschool.com') do |admin|
+  admin.password = 'password123'
+  admin.password_confirmation = 'password123'
+end
+
+puts "Admin user created/verified!"
+
 puts "Seeding Levels..."
 
 levels = [
